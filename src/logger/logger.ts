540a8ad4d3abc031d0359b89a8f3283dadbd667e
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-10-24 01:24:57 -0400

    working on logger

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,5 +1,11 @@
 import { NS } from '@ns';
 
+type LogLevel = 'INFO' | 'WARN' | 'ERROR' | 'CRITICAL';
+type LogMessage = {
+	payload: string;
+	ts: string;
+	msgId: string;
+};
 export class Logger {
 	private ns: NS;
 	private isDebug: boolean;
@@ -15,20 +21,27 @@ export class Logger {
 		this.isTrace = false;
 		this.pprint = true;
 	}
-	logColors(): object {
-		return {
-			BLACK: '\u001b[30m',
-			WHITE: '\u001B[37m',
-			RED: '\u001B[31m',
-			GREEN: '\u001b[32m',
-			MAGENTA: '\u001b[35m',
-			CYAN: '\u001b[36m',
-			YELLOW: '\u001B[33m',
-			BLUE: '\u001B[34m',
-
-			RESET: '\u001B[0m',
-		};
+	set setLogName(logName: string) {
+		if (typeof logName !== 'string') return;
+		this.logName = logName;
+		return;
 	}
+	colors = {
+		BLACK: '\u001b[30m',
+		WHITE: '\u001B[37m',
+		RED: '\u001B[31m',
+		GREEN: '\u001b[32m',
+		MAGENTA: '\u001b[35m',
+		CYAN: '\u001b[36m',
+		YELLOW: '\u001B[33m',
+		BLUE: '\u001B[34m',
+		GREY: '\x1b[38;5;15m',
+		SLATE_BLUE: '\x1b[38;5;33m',
+		DEFAULT_GREEN: '\x1b[38;5;40m',
+		DARK_BROWN: '\x1b[38;5;100m',
+		RED_BG: '\x1b[41m',
+		RESET: '\u001B[0m',
+	};
 }
 
 export async function main(ns: NS) {
