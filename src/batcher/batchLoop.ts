330367fe1feefa74b1001bff587c0c76aec0ecdf
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-10-30 13:27:33 -0400

    Misc Cleanup

--- src/batcher/batchLoop.ts
+++ src/batcher/batchLoop.ts
@@ -4,10 +4,11 @@ import { Logger } from '../logger/logger';
 import { PServer } from '@/util/purchasedServer';
 
 export async function main(ns: NS): Promise<void> {
-	const logger: Logger = new Logger(ns, 'batchLoop');
 	const target: string = (ns.args[0] as string) ?? 'n00dles';
-	const batcher: Batcher = new Batcher(ns, target);
 	const reservedRam: number = (ns.args[1] as number) ?? 20;
+
+	const logger: Logger = new Logger(ns, 'batchLoop');
+	const batcher: Batcher = new Batcher(ns, target);
 	const pServer: PServer = new PServer(ns);
 	while (true) {
 		if (!ns.scriptRunning('dashboard.js', 'home')) return;
