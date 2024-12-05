81dc631f17023f386f7574c638146ea8e6d3de81
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-12-05 18:33:17 -0500

    Add scratch.ts for testing

---
+++ src/scratch.ts
@@ -0,0 +1,8 @@
+/*
+	genJobs(): void {
+		const jobs: BatchJob[] = [];
+		let job = new BatchJob(this.ns);
+		const { ns, ...jobNoNS } = job;
+		return ns.tprint(JSON.stringify(jobNoNS, null, 4));
+	}
+*/
